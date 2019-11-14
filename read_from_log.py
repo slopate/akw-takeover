cdfc4e6b2b0a8df6131b76cbb1ee34f40d349c5b
Alexi Christakis <alexi.christakis@gmail.com>
2019-11-14 15:42:00 -0500

    start simulator code

--- read_from_log.py
+++ read_from_log.py
@@ -2,6 +2,8 @@ import OSC # sudo pip install pyosc (works with py2)
 import serial # sudo pip install pyserial
 import re
 import gpiozero as gp
+import random
+import sys
 import time
 
 
@@ -12,6 +14,33 @@ def parseLogEntry(raw_input):
         return [input['msg']]
     except Exception as e:
         return [raw_input]
+
+def generateSimulatedMessage():
+    val = random.randint(0, 4)
+
+    """
+        format:
+        [NAME]--value--other_value--other_value
+    """
+
+    msg
+    if (val == 0):
+        # generate sarim value
+
+    elif (val == 1):
+        # generate daniel value
+
+    elif (val == 2):
+        # generate sam value
+
+    elif (val == 3):
+        # generate sabrina value
+
+    elif (val == 4):
+        # generate alexi value
+
+    return [msg]
+    
 
 class Listener():
 	osc_client = None
@@ -29,7 +58,7 @@ class Listener():
 
 
 def main():
-    file = open('mesh_logs.log', 'r')
+    mode = sys.argv(0)
 
     c = OSC.OSCClient()
     c.connect(('127.0.0.1', 44444))
@@ -37,20 +66,30 @@ def main():
     listener = Listener()
     listener.add_osc_connect(c)
 
+    if mode == 'sim':
+        # generate data
+        msg = generateSimulatedMessage()
 
-    while True:
-        where = file.tell()
-        line = file.readline()
-        if not line:
-            time.sleep(1)
-            file.seek(where)
-        else:
-            data = parseLogEntry(line)
-            
-            try:
-                listener.sendOSC(data)
-            except Exception as e:
-                print(e)
+        try:
+            listener.sendOSC(msg)
+        except Exception as e:
+            print(e)
+
+    else: 
+       file = open('mesh_logs.log', 'r')
+        while True:
+            where = file.tell()
+            line = file.readline()
+            if not line:
+                time.sleep(1)
+                file.seek(where)
+            else:
+                data = parseLogEntry(line)
+                
+                try:
+                    listener.sendOSC(data)
+                except Exception as e:
+                    print(e)
 
 
 
